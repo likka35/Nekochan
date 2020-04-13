12740bdb491214c3dd4fb5866403a39fe3ce9ac2
Lyrith <lyrith256@gmail.com>
2020-04-14 05:16:15 +0900

    いまいちわかってない

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    }
+};
